07adf0f433e08cc096b160ea0d52beeb259199ea
Saugat Poudel <saugatpoudel2054@gmail.com>
2026-04-18 07:16:38 +0545

    init pre-commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className="flex min-h-full flex-col">
         <ClerkProvider>
           <header className="flex justify-end gap-2 p-4">
             <Show when="signed-out">
